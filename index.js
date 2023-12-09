dc5627916b39b0e06b03cad9f16198a0bbcfc2d8
dazcalifornia <divertzt@gmail.com>
2023-12-09 07:47:53 +0700

    cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { v4: uuidv4 } = require("uuid");
 const app = express();
 
 const corsOptions = {
-  origin: "https://classroom.franx.dev", // Replace with your React app's domain
+  origin: "*", // Replace with your React app's domain
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // Allow credentials (e.g., cookies)
   optionsSuccessStatus: 204,
